89e77b1885da4752dade0bd568d03bdac0313a81
Shivendra Singh <cseshivendra@gmail.com>
2020-11-17 01:25:41 +0800

    Add files via upload

---
+++ SpringMVC_Login/SpringMVC_Login/src/com/mvclogin/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.mvclogin;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/login")
+	public ModelAndView helloWorld(HttpServletRequest request, HttpServletResponse response) {
+		
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		
+		if(password.equals("admin")) {
+			String message = "You are a valid user , Welcome "+name;
+			return new ModelAndView("hellopage","message",message);
+		}
+		else {
+			return new ModelAndView("errorpage","message","Invalid credentials");
+		}
+	}
+
+}
